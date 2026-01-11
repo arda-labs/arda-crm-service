f6b750469bf18624508d0f93d19f179ecf003db6
HoanIT <lehoan.dev@gmail.com>
2026-01-11 20:57:44 +0700

    Initial commit or update

---
+++ src/main/java/vn/io/arda/crm/ArdaCrmServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.io.arda.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArdaCrmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArdaCrmServiceApplication.class, args);
+	}
+
+}
